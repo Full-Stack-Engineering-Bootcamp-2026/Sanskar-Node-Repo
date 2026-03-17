facd57c09548f3480f40b60e9a77a250f1fbe0ad
Sanskar Rajput <sanskar.rajput@mindbowser.com>
2026-03-17 18:45:40 +0530

    feat:add day 2 assignment

---
+++ Day-2/server.js
@@ -0,0 +1,23 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    const { method, url } = req;    
+    const parsedUrl = new URL(url, `http://${req.headers.host}`);
+    const pathName = parsedUrl.pathname;
+    if (pathName == "/api") {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({
+            name: "Sanskar Rajput",
+            date: new Date().toLocaleDateString()
+        }))
+    }
+    else {
+        res.writeHead(200, { "Content-Type": "text/html" })
+        res.end('<div><h1>Sanskar Rajput</h1><h2 id="date"></h2><script>document.getElementById("date").innerHTML = new Date().toLocaleDateString();</script></div>');
+    }
+
+});
+
+server.listen(3000, () => {
+    console.log("Listening at port 3000");
+})
