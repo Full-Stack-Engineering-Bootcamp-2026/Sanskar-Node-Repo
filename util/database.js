6e8ddd1c2517789227e096649535f257d031d7e8
Sanskar Rajput <sanskar.rajput@mindbowser.com>
2026-04-07 15:22:13 +0530

    feat:add day 15 assignment

--- util/database.js
+++ util/database.js
@@ -1,8 +1,18 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+require('dotenv').config();
 
-const sequelize = new Sequelize('node_schema', 'admin', 'Admin@1234', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = callback => {
+  MongoClient.connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.bqfim3v.mongodb.net/?appName=Cluster0`
+  )
+    .then(client => {
+      console.log('Connected!');
+      callback(client);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
