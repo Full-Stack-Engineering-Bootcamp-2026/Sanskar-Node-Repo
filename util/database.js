2f05922f1fca586d535794a3c74625a078650466
Sanskar Rajput <sanskar.rajput@mindbowser.com>
2026-04-07 14:46:51 +0530

    feat:add day 13 assignment

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'admin',
-    database: 'node_schema',
-    password: 'Admin@1234'
+const sequelize = new Sequelize('node_schema', 'admin', 'Admin@1234', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
