51894b9258930e3b2876782d8bab7a31ead3060b
Sanskar Rajput <sanskar.rajput@mindbowser.com>
2026-04-27 12:21:24 +0530

    feat:add day 26 assignment

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,31 @@
-import express, { Request, Response } from "express";
-// import {multer }from 'multer';
+import express, { Request, Response,NextFunction,ErrorRequestHandler } from "express";
 import path from "path";
-
-import { User } from "./modules/user.js";
-import { Order } from "./modules/order.js";
-import { Product } from "./modules/product.js";
+import 'dotenv/config'
 import mongoose from "mongoose";
-import { log } from "node:console";
-import razorpayRouter from './routes/razor-pay.route.js'
-
+import cors from 'cors';
+import 'reflect-metadata';
+import feedRoutes from './routes/feed.route.js'
 const MONGODB_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.bqfim3v.mongodb.net/?appName=Cluster0` as string;
 
 const app = express();
-const port = 3000;
+app.use(cors());
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/images", express.static("images"));
 
-app.use('/payment',razorpayRouter)
-
-
-
+app.use('/feed', feedRoutes);
+app.use((err:any, req:Request, res:Response, next:NextFunction) => {
+  console.log(err);
+  const status = err.statusCode || 500;
+  const message = err.message;
+  res.status(status).json({ message: message });
+});
 const startServer = async () => {
   try {
     await mongoose.connect(MONGODB_URI);
-    app.listen(port);
+    app.listen(port||3000);
     console.log("MongoDB connected successfully!!!");
     console.log("server started at 3000");
   } catch (err) {
